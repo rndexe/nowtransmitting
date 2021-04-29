4044fe4b7c9bcd90751ad41b03caf506cfea6af8
fillerInk <thatonephysicsnerd@gmail.com>
2021-04-29 20:36:24 +0530

    Fixed top navbar

--- src/static/js/exhibit_scroll.js
+++ src/static/js/exhibit_scroll.js
@@ -3,6 +3,7 @@ var exhibit = document.getElementById('exhibit-content')
 var backbutton = document.getElementById("backbutton")
 var navitems = document.getElementById("nav-items-container")
 
+var about = document.querySelector('#about');
 var process = document.querySelector('#process');
 var scholar = document.querySelector('#authors');
 var further = document.querySelector('#takeitfurther')
@@ -11,7 +12,9 @@ var processnav = document.querySelector('#nav-item-process');
 var scholarnav = document.querySelector('#nav-item-scholar');
 var furthernav = document.querySelector('#nav-item-further')
 
-var about = document.querySelector('#about');
+if(process == null){
+	navitems.children[1].removeChild(navitems.children[1].children[2])
+}
 
 backbutton.addEventListener("click",function() {
 	window.location = "#home";
@@ -70,24 +73,31 @@ main.addEventListener('wheel', (e) => {
 		furthernav.children[1].style.visibility = 'hidden'
 	}
 
-	if ((window.scrollX >= about.getBoundingClientRect().left-80) && (window.scrollX <= about.getBoundingClientRect().right-80)) {
+	if ((window.scrollX >= about.getBoundingClientRect().left-80) && (window.scrollX <= scholar.getBoundingClientRect().left-300)) {
 			resetNavbar()
 			aboutnav.children[0].style.color = 'black'
 			aboutnav.children[1].style.visibility = 'visible'
-	}else if ((window.scrollX >= scholar.getBoundingClientRect().left-80) && (window.scrollX <= scholar.getBoundingClientRect().right-80) ) {
+	}else if ((window.scrollX >= scholar.getBoundingClientRect().left-80) && (window.scrollX <= scholar.getBoundingClientRect().right-500) ) {
 		resetNavbar()
 		scholarnav.children[0].style.color = 'white'
 		scholarnav.children[1].style.visibility = 'visible'
 
-	}else if ((window.scrollX >= process.getBoundingClientRect().left-80) && (window.scrollX <= process.getBoundingClientRect().right-80)) {
+	}
+	else if ((window.scrollX >= further.getBoundingClientRect().left-300) && (window.scrollX <= further.getBoundingClientRect().right-80 )) {
+		resetNavbar()
+		furthernav.children[0].style.color = 'black'
+		furthernav.children[1].style.visibility = 'visible'
+	}
+	else if((window.scrollX>=scholar.getBoundingClientRect().right) && process!= null ){
+		if((window.scrollX >= process.getBoundingClientRect().left-80) && (window.scrollX <= further.getBoundingClientRect().left))
+		{	
 			resetNavbar()
 			processnav.children[0].style.color = 'red'
 			processnav.children[1].style.visibility = 'visible'
-
-	}else if ((window.scrollX >= further.getBoundingClientRect().left-80) && (window.scrollX <= further.getBoundingClientRect().right-80 )) {
-			resetNavbar()
-			furthernav.children[0].style.color = 'black'
-			furthernav.children[1].style.visibility = 'visible'
+		}
+	}
+	else{
+		resetNavbar()
 	}
 		
 });
@@ -104,24 +114,3 @@ function resetNavbar(){
 		scholarnav.children[1].style.visibility = 'hidden'
 		furthernav.children[1].style.visibility = 'hidden'
 }
-
-//document.addEventListener('wheel', () => {
-	
-//})
-
-
-//For navigation by clicking and dragging
-
-
-var mobilemenu = document.getElementById("mobile-menu");
-var menubutton = document.getElementById("menu-button");
-var menuclosebutton = document.getElementById("menu-close-button");
-
-menubutton.addEventListener('click',function(){
-	mobilemenu.style.visibility = 'visible'
-});
-
-menuclosebutton.addEventListener('click',function(){
-	mobilemenu.style.visibility = 'hidden'
-});
-
