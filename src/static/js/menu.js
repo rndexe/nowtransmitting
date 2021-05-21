97f48e0e8c8c54c7ae7c65301e8c136d63751f4c
rndexe <h_erande@yahoo.co.in>
2021-05-21 16:56:51 +0530

    about mobile design

--- src/static/js/menu.js
+++ src/static/js/menu.js
@@ -1,5 +1,7 @@
 var menubutton = document.getElementById("menu-button");
 var menuclosebutton = document.getElementById("menu-close-button");
+var menubutton1 = document.getElementById("menu-button1");
+var menuclosebutton1 = document.getElementById("menu-close-button1");
 var menuContainer = document.getElementById('menu-container');
 // things to hide when opening menu
 var infoButton = document.getElementById('infobutton');
@@ -16,6 +18,25 @@ menubutton.addEventListener('click',function(){
 });
 
 menuclosebutton.addEventListener('click',function(){
+    // adding the effects
+    menuContainer.classList.toggle('effect');
+    menuContainer.classList.toggle('menu-open');
+    // menubutton.classList.toggle('hidden');
+    infoButton.classList.toggle('hidden');
+    mediator.classList.toggle('hidden');
+});
+
+menubutton1.addEventListener('click',function(){
+    // adding the effects
+    menuContainer.classList.toggle('effect');
+    menuContainer.classList.toggle('menu-open');
+    // menubutton.classList.toggle('hidden');
+    infoButton.classList.toggle('hidden');
+    mediator.classList.toggle('hidden');
+
+});
+
+menuclosebutton1.addEventListener('click',function(){
     // adding the effects
     menuContainer.classList.toggle('effect');
     menuContainer.classList.toggle('menu-open');
