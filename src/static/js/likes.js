f91644839cd8bb530ef26e8da21174ef87602763
rndexe <h_erande@yahoo.co.in>
2021-05-25 18:11:32 +0530

    database ref firebase

--- src/static/js/likes.js
+++ src/static/js/likes.js
@@ -1,19 +1,31 @@
 
 var likeBut = document.getElementById("likeButton");
-var page=location.pathname;
-var liked=false;
-if (localStorage.getItem(page)) {
-    
-    if(page) {
+var page=location.pathname.split("/").pop();
+var liked = localStorage.getItem(page);
+
+if (liked){
         likeButton.classList.add("bg-red-650");
-    }
+} else {
+    liked = false;
 }
+
 likeBut.addEventListener("click", function() {
-    console.log(page + "liked");
-    localStorage.setItem(page,true)
+    
+    liked = !liked;
+    localStorage.setItem(page,liked)
     likeButton.classList.toggle("bg-red-650");
-
-
+    updateCounter(liked);
 });
+
+function updateCounter(liked) {
+    const db = firebase.database();
+    const updates = {};
+    if (liked) {
+        update[`likes/${page}/`] = firebase.database.ServerValue.increment(1);
+    } else {
+        update[`likes/${page}/`] = firebase.database.ServerValue.increment(-1);
+    }    
+    return db.ref().update(updates);
+}
 
 
